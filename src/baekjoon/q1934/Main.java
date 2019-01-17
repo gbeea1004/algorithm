3d640e8c37721798ff27cc2843b162d9410c0ba7
geonHee <corqkrtk3@naver.com>
2019-01-17 23:07:23 +0900

    error

--- src/baekjoon/q1934/Main.java
+++ src/baekjoon/q1934/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class Main {
     // 최소 공배수 구하기
-    // 다시 풀어보기
+    // 다시 풀어보기!!
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
