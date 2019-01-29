1fbc6173a3dc6474e87baa5f8ee917aab34a9174
geonHee <corqkrtk3@naver.com>
2019-01-29 22:13:36 +0900

    카카오신입공채 1차코테 - 비밀지도

---
+++ src/kakao/SecretMap.java
@@ -0,0 +1,20 @@
+package kakao;
+
+public class SecretMap {
+    // 비밀 지도 (난이도 : 하)
+    public static void main(String[] args) {
+
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        for (int i = 0; i < n; i++) {
+            int target = 1;
+            int arr = arr1[i] | arr2[i];
+            for (int j = 0; j < n; j++) {
+                System.out.print((arr & target) > 0 ? "#" : " ");
+                target = target << 1;
+            }
+            System.out.println();
+        }
+    }
+}
