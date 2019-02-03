7c662f645f5717b35ff8494050abfa6da414eec7
geonHee <corqkrtk3@naver.com>
2019-02-04 01:33:13 +0900

    프로그래머스 level 1 - 이상한  문자 만들기 (다시 풀어서 맞춤)

--- src/programmers/q12930/Main.java
+++ src/programmers/q12930/Main.java
@@ -1,24 +1,32 @@
 package programmers.q12930;
 
-// 다시풀기
+// 주의 > split(" ") 로 하면 틀림. isEven을 통해 짝수인지 홀수인지 판별하여 문제를 푼다.
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = "try hello world";
+        String s = "A v    a";
         System.out.println(solution.solution(s));
     }
 }
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        int cnt = 0;
-        String[] array = s.split("");
-
-        for(String ss : array) {
-            cnt = ss.contains(" ") ? 0 : cnt + 1;
-            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase();
+        StringBuilder answer = new StringBuilder();
+        char[] input = s.toCharArray();
+        boolean isEven = true; // 짝수니?
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == ' ') {
+                answer.append(" ");
+                isEven = true;
+            } else {
+                if (isEven) {
+                    answer.append(String.valueOf(input[i]).toUpperCase());
+                } else {
+                    answer.append(String.valueOf(input[i]).toLowerCase());
+                }
+                isEven = !isEven;
+            }
         }
-        return answer;
+        return answer.toString();
     }
 }
