c7f2b1a6f408e2760255339f6e4ff067cfa88e08
geonHee <corqkrtk3@naver.com>
2019-02-20 18:11:14 +0900

    프로그래머스 level 1 - 정수 제곱근 판별(다른 사람 풀이)

--- src/programmers/q12934/Main.java
+++ src/programmers/q12934/Main.java
@@ -7,16 +7,28 @@ public class Main {
     }
 }
 
+// 다른 사람이 푼 방법
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        double checker = Math.sqrt(n);
-        if (checker - (long)checker == 0.0) {
-            answer = (long)checker + 1;
-            answer *= answer;
-        } else {
-            answer = -1;
+        if (Math.pow((int)Math.sqrt(n), 2) == n) { // Math.pow => 제곱
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
         }
-        return answer;
+
+        return -1;
     }
 }
+
+// 내가 푼 방법
+//class Solution {
+//    public long solution(long n) {
+//        long answer = 0;
+//        double checker = Math.sqrt(n);
+//        if (checker - (long)checker == 0.0) {
+//            answer = (long)checker + 1;
+//            answer *= answer;
+//        } else {
+//            answer = -1;
+//        }
+//        return answer;
+//    }
+//}
