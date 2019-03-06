ef2744b82f345f62fa9bc0ac1989c31a297a059c
geonHee <corqkrtk3@naver.com>
2019-03-07 00:26:09 +0900

    백준 q2775 해결 런타임에러 원인 : 1 14 14 시 배열 범위초과 에러가 뜸 해결 : apartment 2차원 배열의 크기를 new int[15][14] 로 수정

--- src/baekjoon/q2775/Main.java
+++ src/baekjoon/q2775/Main.java
@@ -8,15 +8,15 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int count = Integer.parseInt(br.readLine());
-        int[][] people = new int[14][14];
+        int[][] apartment = new int[15][14];
         for (int i = 0; i < 14; i++) {
-            people[0][i] = i + 1;
+            apartment[0][i] = i + 1;
         }
-        for (int i = 1; i < people.length; i++) {
+        for (int i = 1; i < apartment.length; i++) {
             int sum = 0;
-            for (int j = 0; j < people[i].length; j++) {
-                sum += people[i - 1][j];
-                people[i][j] = sum;
+            for (int j = 0; j < apartment[i].length; j++) {
+                sum += apartment[i - 1][j];
+                apartment[i][j] = sum;
             }
         }
 
@@ -24,7 +24,8 @@ public class Main {
             int k = Integer.parseInt(br.readLine()); // 층
             int n = Integer.parseInt(br.readLine()); // 호
 
-            bw.write(people[k - 1][n] + System.lineSeparator());
+            bw.write(apartment[k][n - 1] + "");
+            bw.write(System.lineSeparator());;
         }
 
         br.close();
