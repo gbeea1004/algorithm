48a9b034519e87ad5356a715222cc5422a6a10b2
geonHee <corqkrtk3@naver.com>
2019-03-13 04:05:13 +0900

    SW 2047 : 신문 헤드라인

---
+++ src/sw_academy/q2047/Solution.java
@@ -0,0 +1,15 @@
+package sw_academy.q2047;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+
+        bw.write(input.toUpperCase());
+        bw.flush();
+    }
+}
