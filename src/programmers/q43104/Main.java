070d961e97824ac83edd44ba816a16e4c572e554
geonHee <corqkrtk3@naver.com>
2019-04-08 13:09:48 +0900

    프로그래머스 LEVEL 3 : 타일 장식물 (DP)

--- src/programmers/q43104/Main.java
+++ src/programmers/q43104/Main.java
@@ -1,8 +1,5 @@
 package programmers.q43104;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -12,15 +9,12 @@ public class Main {
 
 class Solution {
     public long solution(int N) {
-        List<Integer> no = new ArrayList<>();
-        no.add(1);
-        no.add(1);
-
-        for (int i = 2; i < N; i++) {
-            no.add(no.get(i-1) + no.get(i-2));
+        long[] fibonacci = new long[N];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
         }
-        int currentNo = no.get(no.size() - 1);
-        int preNo = no.get(no.size() - 2);
-        return ((currentNo + preNo) + (preNo + no.get(no.size() - 3))) * 2;
+        return fibonacci[N - 1] * 4 + fibonacci[N - 2] * 2;
     }
 }
