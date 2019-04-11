4ba2b3c4a3b57d46ae1d381ccd4a038f19b0d738
geonHee <corqkrtk3@naver.com>
2019-04-11 17:35:59 +0900

    프로그래머스 LEVEL 2 : JadenCase 문자열 만들기

--- src/programmers/q12951/Main.java
+++ src/programmers/q12951/Main.java
@@ -3,20 +3,22 @@ package programmers.q12951;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution("3people unFollowed me"));
+        System.out.println(solution.solution("1 2 3 "));
 //        System.out.println(solution.solution("for the last week"));
     }
 }
 
 class Solution {
     public String solution(String s) {
-        String[] input = s.split(" ");
+        String[] input = s.split(" ", -1);
         StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < input.length; i++) {
             String text = input[i];
-            answer.append(text.substring(0, 1).toUpperCase());
-            answer.append(text.substring(1).toLowerCase());
+            if (text.length() > 0) {
+                answer.append(text.substring(0, 1).toUpperCase());
+                answer.append(text.substring(1).toLowerCase());
+            }
             if (i == input.length - 1) {
                 break;
             }
