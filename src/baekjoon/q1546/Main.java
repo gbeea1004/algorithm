d1bbb238007977e55d6c2a54ccfa70722f1ae53b
gunny <corqkrtk3@naver.com>
2019-04-21 23:14:23 +0900

    백준 : 평균 (q1546) 해결 : total 변수로 모든 점수를 저장해 두고 한번에 평균 구하기 Formatter 클래스를 사용하여 결과값을 원하는 형식으로 만듬

--- src/baekjoon/q1546/Main.java
+++ src/baekjoon/q1546/Main.java
@@ -1,3 +1,30 @@
 package baekjoon.q1546;
 
+import java.io.*;
+import java.util.Formatter;
+import java.util.stream.Stream;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        int[] score = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int max = score[0];
+        int total = score[0];
+        for (int i = 1; i < score.length; i++) {
+            total += score[i];
+            if (max < score[i]) {
+                max = score[i];
+            }
+        }
+
+        double avg = total * 100.0 / max  / count;
+        Formatter f = new Formatter();
+        f.format("%.2f", avg);
+        bw.write(String.valueOf(f));
+
+        bw.flush();
+    }
+}
