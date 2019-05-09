cabe89eb9a176953aa3fd7484699970a08f960b0
geonHee <corqkrtk3@naver.com>
2019-05-09 13:23:50 +0900

    SW Expert Academy : 상원이의 연속 합 (q7510) 오류 : 제한시간 초과 왜? : 재귀를 사용하니 구하려는 값이 커질수록 엄청난 재귀로 메모리를 사용하여 StackOverFlow가 남

---
+++ src/main/java/sw_academy/q7510/Solution.java
@@ -0,0 +1,43 @@
+package sw_academy.q7510;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= count; i++) {
+            int no = Integer.parseInt(br.readLine());
+            if (no == 1) {
+                bw.write("#" + i + " " + 1);
+                bw.newLine();
+                continue;
+            }
+
+            int result = 1;
+            boolean isEnd = false;
+            for (int j = no / 2 + 1; j >= 1 ; j--) {
+                int sum = 0;
+                for (int k = j; k >= 1; k--) {
+                    sum += k;
+                    if (sum == no) {
+                        if (k == 1) {
+                            isEnd = true;
+                        }
+                        result++;
+                        break;
+                    }
+                }
+
+                if (isEnd) {
+                    break;
+                }
+            }
+            bw.write("#" + i + " " + result);
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
