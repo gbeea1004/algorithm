ecffc7e9a8a8a3327198702e9dbe88e0b6f3a44e
gunny <corqkrtk3@naver.com>
2019-06-21 22:32:44 +0900

    프로그래머스 : KAKAO 오픈채팅방

--- src/main/java/programmers/q42888/Solution.java
+++ src/main/java/programmers/q42888/Solution.java
@@ -6,68 +6,51 @@ import java.util.List;
 import java.util.Map;
 
 public class Solution {
+    private final String ENTER_MSG = "님이 들어왔습니다.";
+    private final String GO_OUT_MSG = "님이 나갔습니다.";
+    private final String ENTER = "Enter";
+    private final String LEAVE = "Leave";
+    private final String CHANGE = "Change";
+
     private Map<String, String> users = new HashMap<>();
+    private List<User> chattingRoom = new ArrayList<>();
 
     public String[] solution(String[] record) {
-        List<User> chattingRoomUsers = new ArrayList<>();
-
-        for (String text : record) {
-            String[] control = text.split(" ");
-            modify(chattingRoomUsers, control);
+        for (String texts : record) {
+            String[] text = texts.split(" ");
+            if (text[0].equals(ENTER)) {
+                users.put(text[1], text[2]);
+                chattingRoom.add(new User(text[0], text[1]));
+                continue;
+            }
+            if (text[0].equals(LEAVE)) {
+                chattingRoom.add(new User(text[0], text[1]));
+                continue;
+            }
+            if (text[0].equals(CHANGE)) {
+                users.put(text[1], text[2]);
+            }
         }
 
-        String[] answer = new String[chattingRoomUsers.size()];
+        String[] answer = new String[chattingRoom.size()];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = messagePrint(chattingRoomUsers.get(i));
+            StringBuilder sb = new StringBuilder();
+            if (chattingRoom.get(i).STATUS.equals(ENTER)) {
+                answer[i] = sb.append(users.get(chattingRoom.get(i).ID)).append(ENTER_MSG).toString();
+                continue;
+            }
+            answer[i] = sb.append(users.get(chattingRoom.get(i).ID)).append(GO_OUT_MSG).toString();
         }
         return answer;
     }
 
-    private String messagePrint(User user) {
-        StringBuilder sb = new StringBuilder();
-        if (user.status.equals("Enter")) {
-            return sb.append(user.name).append("님이 들어왔습니다.").toString();
-        }
-        return sb.append(user.name).append("님이 나갔습니다.").toString();
-    }
-
-    private void modify(List<User> chattingRoomUsers, String[] control) {
-        if (control[0].equals("Enter")) {
-            users.put(control[1], control[2]);
-            chattingRoomUsers.add(new User(control[0], control[1], control[2]));
-            for (User user : chattingRoomUsers) {
-                user.changeName(control[1], control[2]);
-            }
-            return;
-        }
-
-        if (control[0].equals("Leave")) {
-            chattingRoomUsers.add(new User(control[0], control[1], users.get(control[1])));
-            return;
-        }
-
-        if (control[0].equals("Change")) {
-            for (User user : chattingRoomUsers) {
-                user.changeName(control[1], control[2]);
-            }
-        }
-    }
-
     private class User {
-        private final String id;
-        private final String status;
-        private String name;
+        private final String STATUS;
+        private final String ID;
 
-        public User(String status, String id, String name) {
-            this.status = status;
-            this.id = id;
-            this.name = name;
-        }
-
-        public void changeName(String id, String changedName) {
-            if (this.id.equals(id)) {
-                name = changedName;
-            }
+        public User(String STATUS, String ID) {
+            this.STATUS = STATUS;
+            this.ID = ID;
         }
     }
 }
