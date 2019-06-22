dbfc8fbdd37ebcb51b3162725d14b8b4f7661e9b
gunny <corqkrtk3@naver.com>
2019-06-23 00:04:32 +0900

    프로그래머스 : KAKAO 실패율 실패 이유 : 런타임 에러 코드 설계를 잘못 짠건가??

---
+++ src/test/java/programmers/q42889/SolutionTest.java
@@ -0,0 +1,26 @@
+package programmers.q42889;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SolutionTest {
+    private Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testCase_1() {
+        assertThat(solution.solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3})).isEqualTo(new int[]{3, 4, 2, 1, 5});
+    }
+
+    @Test
+    public void testCase_2() {
+        assertThat(solution.solution(4, new int[]{4, 4, 4, 4, 4})).isEqualTo(new int[]{4, 1, 2, 3});
+    }
+}
