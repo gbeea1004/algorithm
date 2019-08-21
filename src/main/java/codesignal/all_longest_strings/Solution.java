7679ff17a5b8826a778ebfeead70e7ebd460594f
geonHee <corqkrtk3@naver.com>
2019-08-21 20:20:31 +0900

    코드시그널 : All Longest Strings

---
+++ src/main/java/codesignal/all_longest_strings/Solution.java
@@ -0,0 +1,23 @@
+package codesignal.all_longest_strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    String[] allLongestStrings(String[] inputArray) {
+        int maxLength = 0;
+        for (String text : inputArray) {
+            if (text.length() > maxLength) {
+                maxLength = text.length();
+            }
+        }
+
+        List<String> texts = new ArrayList<>();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (maxLength == inputArray[i].length()) {
+                texts.add(inputArray[i]);
+            }
+        }
+        return texts.toArray(new String[texts.size()]);
+    }
+}
