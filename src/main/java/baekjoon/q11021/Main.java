8f80dbbe1840c2e4bb356e83b6137a49aafce882
gunny <corqkrtk3@naver.com>
2019-08-27 15:21:31 +0900

    백준 : A + B - 7

---
+++ src/main/java/baekjoon/q11021/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.q11021;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = scanner.nextInt();
+        for (int i = 1; i <= count; i++) {
+            bw.write("Case #" + i + ": " + (scanner.nextInt() + scanner.nextInt()));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
