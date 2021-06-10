4969fcf64e9706e882709b11036f6af26decd622
gbeea1004 <jvckigh@gmail.com>
2021-06-11 01:20:01 +0900

    백준: 피보나치 수 5

---
+++ src/main/java/baekjoon/q10870/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.q10870;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(new Solution().solution(n));
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
