6737ccaf72287a7cf029a2db7cdf0e0b3e4e7f1e
my <jvckigh@gmail.com>
2022-05-06 20:30:30 +0900

    프로그래머스: 제일 작은 수 제거하기

--- src/main/java/programmers/q12935/Solution.java
+++ src/main/java/programmers/q12935/Solution.java
@@ -1,2 +1,26 @@
-package programmers.q12935;public class Solution {
+package programmers.q12935;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+        int minNo = Arrays.stream(arr)
+                          .min()
+                          .getAsInt();
+        int[] result = new int[arr.length - 1];
+        boolean findMinNo = false;
+        int resultIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (!findMinNo && arr[i] == minNo) {
+                findMinNo = true;
+                continue;
+            }
+            result[resultIndex++] = arr[i];
+        }
+        return result;
+    }
 }
