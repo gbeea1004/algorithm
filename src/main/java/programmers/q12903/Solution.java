2486fa597246d2da910c8ecc158a59e7a7cb3deb
my <jvckigh@gmail.com>
2022-05-13 06:35:11 +0900

    프로그래머스: 가운데 글자 가져오기

---
+++ src/main/java/programmers/q12903/Solution.java
@@ -0,0 +1,10 @@
+package programmers.q12903;
+
+public class Solution {
+    public String solution(String s) {
+        if (s.length() % 2 == 0) {
+            return s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+        }
+        return String.valueOf(s.charAt(s.length() / 2));
+    }
+}
