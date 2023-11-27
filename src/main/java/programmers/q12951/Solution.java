aaecf9a86e49be951af0e1df5958a495583687f8
1000338 <lime@onestorecorp.com>
2023-11-27 18:37:11 +0900

    프로그래머스: JadenCase 문자열 만들기

---
+++ src/main/java/programmers/q12951/Solution.java
@@ -0,0 +1,17 @@
+package programmers.q12951;
+
+public class Solution {
+
+  public String solution(String s) {
+    String answer = "";
+    String[] words = s.toLowerCase().split("");
+    boolean isBlank = true;
+
+    for (String word : words) {
+      answer += isBlank ? word.toUpperCase() : word;
+      isBlank = word.equals(" ");
+    }
+
+    return answer;
+  }
+}
