4d8167c5d77a1cfb6114882d9eaa35db35b663f7
geonhee <jvckigh@gmail.com>
2023-12-13 21:59:40 +0900

    프로그래머스: qr code

---
+++ src/main/java/programmers/q181903/Solution.java
@@ -0,0 +1,17 @@
+package programmers.q181903;
+
+public class Solution {
+
+  public String solution(int q, int r, String code) {
+    String answer = "";
+
+    String[] codes = code.split("");
+    for (int i = 0; i < codes.length; i++) {
+      if (i % q == r) {
+        answer += codes[i];
+      }
+    }
+
+    return answer;
+  }
+}
